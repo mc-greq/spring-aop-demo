3210b17dd83a6c4a27abf098756945c36e48e04c
mc-greq <greq19@gmail.com>
2019-02-21 13:08:05 +0100

    Basic setup done.

--- src/example/nio/SpringAopRun.java
+++ src/example/nio/SpringAopRun.java
@@ -1,9 +1,24 @@
 package example.nio;
 
 
+import example.nio.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SpringAopRun {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // read spring configuration java class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from container
+        AccountDAO accountDAO = context.getBean(AccountDAO.class);
+
+        // call the business method
+        accountDAO.addAccount();
+
+        // close the context
+        context.close();
     }
 }
